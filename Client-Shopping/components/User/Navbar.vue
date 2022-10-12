b3eefdb02995a9819ad9a9cb2227c3a690c445d2
dangtoan2205 <dnthauk18@gmail.com>
2022-10-12 16:31:47 +0700

    do an thuc tap tot nghiep dang ngoc toan 18cn2 10/2022

---
+++ Client-Shopping/components/User/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light scrolled awake" id="ftco-navbar">
+    <div class="container">
+      <nuxt-link to="/" class="navbar-brand nuxt-link-active" style="color: #82ae46;">Vegefoods</nuxt-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <nuxt-link to="/" class="nav-link nav-hover">Home</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/shop" class="nav-link">Shop</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/" class="nav-link">About</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/" class="nav-link">Blog</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/" class="nav-link">Contact</nuxt-link>
+          </li>
+          <li class="nav-item cta-colored">
+            <nuxt-link class="nav-link" :to="{ name: 'cart' }">
+              <span class="icon-shopping_cart"></span>
+              <span  v-if="cartQuantity > 0">[{{ cartQuantity }}]</span>
+            </nuxt-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Accout</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown04" v-if="this.$store.state.user.token !== null">
+              <a href="" class="dropdown-item">{{ getUser.name }}</a>
+              <a href="#" class="dropdown-item" @click="logout">Logout</a>
+            </div>
+
+            <div class="dropdown-menu" aria-labelledby="dropdown04" v-if="this.$store.state.user.token == null">
+              <nuxt-link to="/login" class="dropdown-item">Login</nuxt-link>
+              <nuxt-link to="/register" class="dropdown-item">Register</nuxt-link>
+            </div>
+
+<!--            <div class="dropdown-menu" aria-labelledby="dropdown04">-->
+<!--              <nuxt-link to="/login" class="dropdown-item" v-show="this.$store.state.user.token">Login</nuxt-link>-->
+<!--              <a href="#" class="dropdown-item" v-show="this.$store.state.user.token !== null" @click="logout">Logout</a>-->
+<!--            </div>-->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'UserNavbar',
+  computed: {
+    ...mapGetters(['cartQuantity', 'getUser']),
+
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    },
+  },
+}
+</script>
---
+++ Client-Shopping/components/admin/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <div class="hidden w-full md:block md:w-auto">
+        <a href="#" class="block w-32">
+          <img src="/assets/img/logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" style="height: 50px"/>
+        </a>
+      </div>
+      <div class="hidden w-full md:block md:w-auto">
+        <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 ">
+          <li>
+            <nuxt-link :to="{ name: 'admin-category' }">
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Category
+              </a>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{ name: 'admin-tag' }">
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Tag
+              </a>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{ name: 'admin-product' }">
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Product
+              </a>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link :to="{ name: 'admin-order' }">
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                Oder
+              </a>
+            </nuxt-link>
+          </li>
+          <li>
+            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" @click="logout">
+              Log out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+</script>
